f6bfcc90cd06cd94bd02ce4f8bb6ff785dae627f
jianyong.jiang <jjy_8888@163.com>
2017-07-26 17:50:09 +0800

    udp测试

---
+++ TestBasicNetObject/UdpClientA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpClientA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new UdpClient(8888))
+            {
+                while(true)
+                {
+                    
+                }
+            }
+        }
+    }
+}
