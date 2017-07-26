92b7bed7d48c358e0088940a0f590468b9480362
jianyong.jiang <jjy_8888@163.com>
2017-07-26 16:59:18 +0800

    基本网络对象测试

---
+++ TestBasicNetObject/TestBasicNetObject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TestBasicNetObject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var localHostName = Dns.GetHostName();
+            var ips = Dns.GetHostAddressesAsync(Dns.GetHostName()).Result;
+
+            Console.WriteLine($"local host name:{localHostName}");
+            foreach(var ip in ips)
+            {
+                Console.WriteLine($"ip address:{ip}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
