91af7ce3f2aab3a44d6c5ef162eb40a43ab858ea
jack <jjy_8888@163.com>
2017-08-16 00:55:18 +0800

    增加TCP 测试demo

---
+++ TestBasicNetObject/TcpClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Connect(IPAddress.Loopback, 4000);
+                var sendBytes = Encoding.UTF8.GetBytes("hello tcp application!");
+                var dataSize = BitConverter.GetBytes(sendBytes.Length);
+                using (var netStream = new NetworkStream(socket))
+                {
+                    //tcp 消息边界 
+                    /*
+                     1. 发送固定长度消息
+                     2. 将消息长度和消息一起发送
+                     3. 使用特殊标记分隔消息
+                     */
+                    netStream.Write(dataSize, 0, dataSize.Length);
+                    netStream.Write(sendBytes, 0, sendBytes.Length);
+                    netStream.Flush();
+                }
+            }
+            Console.WriteLine("send completed.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestBasicNetObject/TcpServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Bind(new IPEndPoint(IPAddress.Loopback, 4000));
+                socket.Listen(10);
+                while (true)
+                {
+                    var newSocket = socket.Accept();
+                    Task.Factory.StartNew(AcceptMessage, newSocket);
+                }
+            }  
+        }
+
+        static void AcceptMessage(object state)
+        {
+            var newSocket = (Socket)state;
+            if (newSocket.Connected)
+            {
+                Console.WriteLine($"from {newSocket.RemoteEndPoint} connect...");
+                using (var netStream = new NetworkStream(newSocket))
+                {
+                    var dateSize = new byte[4];
+                    netStream.Read(dateSize, 0, 4);
+                    int size = BitConverter.ToInt32(dateSize, 0);
+                    var message = new byte[size];
+                    int dataLeft = size;
+                    int start = 0;
+                    while(dataLeft > 0)
+                    {
+                        int recv = netStream.Read(message, start, dataLeft);
+                        start += recv;
+                        dataLeft -= recv;
+                    }
+                    Console.WriteLine($"accept content:{Encoding.UTF8.GetString(message)}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"from {newSocket.RemoteEndPoint}  connect failed.");
+            }
+        }
+    }
+}
